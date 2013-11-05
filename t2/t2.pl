0551dd89dbfa6a309cda0ba462bfa0e593b9ebd7
lwf09 <leonardowistuba@gmail.com>
2013-11-04 22:16:23 -0200

    conexoes com

--- t2/t2.pl
+++ t2/t2.pl
@@ -1,3 +1,36 @@
 % Aluno: Leonardo Wistuba de França
-% GRR: 2093551
+% GRR: 20093551
+conexoesCom(CidadeA, [], []).
+conexoesCom(CidadeA, [[WO, WD, WC]|T], [[WO, WD, WC]|Conexoes]):-
+	conexoesCom(CidadeA, T, Conexoes),
+	WO is CidadeA,
+	!.
+conexoesCom(CidadeA, [[WO, WD, WC]|T], [[WO, WD, WC]|Conexoes]):-
+	conexoesCom(CidadeA, T, Conexoes),
+	WD is CidadeA,
+	!.
 
+conexoesCom(CidadeA, [[WO, WD, WC]|T], Conexoes):-
+	conexoesCom(CidadeA, T, Conexoes).
+	
+
+vizinhoMaisProximo(CidadeA, [H|T], ListaVisitados, MaisProximo, Custo).
+
+
+
+
+
+
+
+custo(CidadeA, CidadeB, [H|_], Custo):-
+	splitCusto(H, CidadeA, CidadeB, Custo),
+	!.
+custo(CidadeA, CidadeB, [_|T], Custo):-
+	custo(CidadeA, CidadeB, T, Custo).
+
+splitCusto([CidadeA, CidadeB, Custo], CidadeA, CidadeB, Custo).
+splitCusto([CidadeA, CidadeB, Custo], CidadeB, CidadeA, Custo).
+
+aleatorio(R):-
+	R is random(60) + 1.
+	
