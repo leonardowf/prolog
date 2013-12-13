a242a927dca0c138d08c3f725b1f0b1e00492fee
lwf09 <leonardowistuba@gmail.com>
2013-12-13 21:04:56 -0200

    Assinatura das heurísticas

--- t3/a.pl
+++ t3/a.pl
@@ -1,2 +1,18 @@
 % Aluno: Leonardo Wistuba de França
 % GRR: 20093551
+
+heuristica_horizontal(Lista, Total):-
+	Total is 10.
+
+heuristica_vertical(Lista, Total):-
+	Total is 20.
+
+heuristica_diagonal(Lista, Total):-
+	Total is 30.
+
+heuristica(Lista, Total):-
+	heuristica_horizontal(Lista, TotalHorizontal),
+	heuristica_vertical(Lista, TotalVertical),
+	heuristica_diagonal(Lista, TotalDiagonal),
+	Parcial is TotalHorizontal + TotalVertical,
+	Total is Parcial + TotalDiagonal.
